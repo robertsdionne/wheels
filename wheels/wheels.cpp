26ce5ae1423442ae7c13b23d616a975dc99d5403
Robert Scott Dionne <robertsdionne@gmail.com>
2016-01-18 18:15:55 -0500

    Output a vector.

--- wheels/wheels.cpp
+++ wheels/wheels.cpp
@@ -2,11 +2,15 @@
 
 #include "libraries/glfw/include/GLFW/glfw3.h"
 #include "libraries/glm/glm/glm.hpp"
+#include "libraries/glm/glm/gtx/io.hpp"
 
 int main(int argument_count, char *arguments[]) {
   if (!glfwInit()) {
     return -1;
   }
   glm::vec3 v;
+
+  std::cout << v << std::endl;
+
   return 0;
 }
